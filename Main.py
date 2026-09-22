c33f0ab384778e6b2b91fb398a85da0b1dba4a9c
Sashadudkin <sasha.dudkin@stu.ocsb.ca>
2026-09-22 11:14:13 -0400

    I did it!

--- Main.py
+++ Main.py
@@ -8,21 +8,44 @@
 #############################################
 
 # THIS IS WHERE YOU CODE
-#name of the first team
+
 print("What's first team name?")
 team_1_name = input()
-#first team's wins
+#
 print("How many wins does first team have?")
 team_1_wins: int = int(input())
-#first team's ties
+#
 print("How many ties does first team have?")
 team_1_ties = int(input())
-#frist team's loses
+#
 print("How many losses does first team have?")
 team_1_losses = int(input())
-#wins are multiplied by 2,
-#Ties by multiplied 1 (not multiplied if easier)
-#And loses doesn't count.
-#Wins, ties and loses are adding on each other.
-print(team_1_name,"'s score is", team_1_score)
-team_1_score = team_1_wins * 2 + team_1_ties * 1
+#
+team_1_score: int = (team_1_wins * 2) + (team_1_ties * 1)
+print(team_1_name,"score is",team_1_score)
+
+print("It's time for second team!")
+
+print("What's second team's name?")
+team_2_name = input()
+#
+print("How many wins does second team have?")
+team_2_wins: int = int(input())
+#
+print("How many ties does second team have?")
+team_2_ties: int = int(input())
+#
+print("How many losses does second team have?")
+team_2_losses: int = int(input())
+#
+team_2_score = (team_2_wins * 2) + (team_2_ties * 1)
+print(team_2_name,"score is",team_2_score)
+
+if (team_1_score > team_2_score):
+    print(team_1_name, "wins!!!")
+
+elif (team_1_score < team_2_score):
+    print(team_2_name, "wins!!!")
+
+elif team_1_score==team_2_score:
+    print("TIE!")
